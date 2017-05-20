f2ba6bb0f11bdfd01897fa00582d41818df82ae5
Wamae Keige <mkeige@gmail.com>
2017-05-20 09:58:49 +0300

    tasklist

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+<title>Laravel QuickStart -Basic</title>
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-default">
+</nav>
+</div>
+@yield('content')
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
